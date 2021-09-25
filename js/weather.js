e60900ddfca445ab48bf06e864239ba8a9954a28
jihoon <hun2ya9@gmail.com>
2021-09-25 23:02:04 +0900

    weather(날씨) 추가
    https://openweathermap.org 의 API 사용하여 현재 위치의 날씨/온도 보여줌

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "bf7ea2c54a1866f859149e84d3d59735";
+
+function onGeoOk(position){
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log(lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    city.innerText = data.name;
+  });
+}
+
+function onGeoError(){
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
