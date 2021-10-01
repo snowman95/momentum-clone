eca00c5a83c9d36f10b2f2e41bf0f28b0214798c
jihoon <hun2ya9@gmail.com>
2021-10-01 23:23:57 +0900

    새 배경이미지 추가

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpg", "4.jpg", "5.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
